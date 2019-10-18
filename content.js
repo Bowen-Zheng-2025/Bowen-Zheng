6221341d7c9e9e5a69710e8c23c376a946a81925
Bowen Zheng <bowenzheng2003@gmail.com>
2019-10-18 10:15:48 -0400

    hi brad

---
+++ content.js
@@ -0,0 +1,70 @@
+/*
+Making the content.js file -
+
+It should contain a single constant named content.
+This constant should contain all of the statements we came up with, one per line, with their true or false state separated by a semicolon. For example:
+const content = `Aidan started the year doing well; true
+Aidan is still doing well; false`;
+*/
+Jake is smart
+The Earth is Flat
+Jake can't not roste
+Working in this class is helps
+Pluto is not a normal planet
+Global warming isnt real
+bowen is not not not not not not smart
+Jake is a good driver
+The sun is hot
+Aidan is not lazy
+when working with Aidan litle work gets done
+Mike isnt a total beast with monty in ros
+piggins were made by the government
+bad dragonis comoto dragon breeding company with the goal of making them a domesticated pet
+The earth is not flat
+The earth is not a perfect sphere
+Radiation kills
+Mike can spell much good
+global warming is bad
+memphis = kim john un
+fire herts
+aidan = bad at siege
+memphis is secretly tall but has prosthetic legs
+Aidan noob rages on rainbow 6 siege (and sucks at it)
+euan is memphis's twin and took his legs
+it smells like stale diretos
+Emma is tall
+it is raining outside
+Aidan is an idiot
+Bowen is cool
+Memphis cant reach the top shelf
+lamorie has baby legs
+bowen has aidans wallet
+Mike cant spell refrigir8tors
+mr malorie secretly works for nasa
+jake has a shark neckless
+jake like pizza
+it is still summer
+jakes computer is a dcu
+aidan has a youtube channel called blaze conroy
+sharks have arms
+Bowen doesnt have glasses
+kade has a crush on brad
+euan is swim
+mike is trash
+aidan is the better leader
+aidan has a future
+TBB is best
+Lamorie is cool
+mike is here legaly
+alienwear is a good brand
+the sky is blue
+Lamorie has baby legs
+Jake is a noob
+PC masterace
+jake is a good driver
+Mike is not a squeaker
+aidan has a wallet
+gaming laptops are good
+mike can do basic motor functions
+siege has small maps
+mike is cool
