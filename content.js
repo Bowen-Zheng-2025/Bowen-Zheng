c2318a8679cf3d5fdb4b7c6d3cccd1e2eaad43be
Bowen Zheng <bowenzheng2003@gmail.com>
2019-10-18 10:36:31 -0400

    Finished the content.js

--- content.js
+++ content.js
@@ -6,65 +6,65 @@ This constant should contain all of the statements we came up with, one per line
 const content = `Aidan started the year doing well; true
 Aidan is still doing well; false`;
 */
-Jake is smart
-The Earth is Flat
-Jake can't not roste
-Working in this class is helps
-Pluto is not a normal planet
-Global warming isn't real
-bowen is not not not not not not smart
-Jake is a good driver
-The sun is hot
-Aidan is not lazy
-when working with Aidan litle work gets done
-Mike isnt a total beast with monty in ros
-piggins were made by the government
-bad dragonis comoto dragon breeding company with the goal of making them a domesticated pet
-The earth is not flat
-The earth is not a perfect sphere
-Radiation kills
-Mike can spell much good
-global warming is bad
-memphis = kim john un
-fire herts
-aidan = bad at siege
-memphis is secretly tall but has prosthetic legs
-Aidan noob rages on rainbow 6 siege (and sucks at it)
-euan is memphis's twin and took his legs
-it smells like stale diretos
-Emma is tall
-it is raining outside
-Aidan is an idiot
-Bowen is cool
-Memphis cant reach the top shelf
-lamorie has baby legs
-bowen has aidans wallet
-Mike cant spell refrigir8tors
-mr malorie secretly works for nasa
-jake has a shark neckless
-jake like pizza
-it is still summer
-jakes computer is a dcu
-aidan has a youtube channel called blaze conroy
-sharks have arms
-Bowen doesnt have glasses
-kade has a crush on brad
-euan is swim
-mike is trash
-aidan is the better leader
-aidan has a future
-TBB is best
-Lamorie is cool
-mike is here legaly
-alienwear is a good brand
-the sky is blue
-Lamorie has baby legs
-Jake is a noob
-PC masterace
-jake is a good driver
-Mike is not a squeaker
-aidan has a wallet
-gaming laptops are good
-mike can do basic motor functions
-siege has small maps
-mike is cool
+const content = `Jake is smart; false
+The Earth is flat; false
+Jake can't not roast; true
+Working in this class is helps; true
+Pluto is not a normal planet; true
+Global warming isn't real; false
+Bowen is not not not not not not smart; true
+Jake is a good driver; false
+The sun is hot; true
+Aidan is not lazy; false
+When working with Aidan, litle work gets done; true
+Mike isn't a total beast with Monty in Ros; true
+Pigeons were made by the government; true
+bad dragon is comoto dragon breeding company with the goal of making them a domesticated pet
+The earth is not flat; true
+The earth is not a perfect sphere; true
+Radiation kills; true
+Mike can spell much good; false
+global warming is bad; true
+memphis = kim john un; true
+Fire hurts; true
+aidan = bad at siege; true
+Memphis is secretly tall but has prosthetic legs; true
+Aidan noob rages on rainbow 6 siege (and sucks at it); false
+Euan is memphis's twin and took his legs; true
+It smells like stale doritos; true
+Emma is tall; false
+It is raining outside; false
+Aidan is an idiot; false
+Bowen is cool; true
+Memphis can't reach the top shelf; true
+Lamorie has baby legs; true
+Bowen has Aidan's wallet; true
+Mike can't spell refrigir8tors; true
+Mr. Malorie secretly works for nasa; true
+Jake has a shark necklace; true
+Jake likes pizza; false
+It is still summer; false
+Jake's computer is a DCU; false
+Aidan has a youtube channel called Blaze Conroy (Go check it out!); true
+Sharks have arms; false
+Bowen doesn't have glasses; false
+Kade has a crush on Brad; true
+Euan can swim; true
+Mike is trash; true
+Aidan is the better leader; true
+Aidan has a future; true
+TBB is best; true
+Lamorie is cool; true
+Mike is here legally; false
+Alienwear is a good brand; false
+The sky is blue; true
+Lamorie has baby legs; true
+Jake is a noob; true
+PC masterace; truw
+Jake is a good driver; false
+Mike is not a squeaker; false
+Aidan has a wallet; false
+Gaming laptops are good; false
+Mike can do basic motor functions; false
+Siege has small maps; false
+Mike is cool; false`;
