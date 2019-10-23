b609ad30f546dbd3df97b75dc5ec18995218e837
Aidan <aidanbab1@gmail.com>
2019-10-23 10:05:58 -0400

    4th function == dun

--- Aidan's.js
+++ Aidan's.js
@@ -1,1 +1,8 @@
 
+/* orEval(condition1, condition2)
+takes two conditions and makes a new condition out of them based on their combined
+or state and combines the text of the two conditions. Cleans up text some
+@param condition1 {object} an object with str and bool keys
+@param condition2 {object} an object with str and bool keys
+@return {object} an object with str and bool keys
+*/
