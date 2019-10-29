74344767e40e7dffaf34ccd4189ccae19fc6be26
Bowen Zheng <bowenzheng2003@gmail.com>
2019-10-29 09:39:37 -0400

    finished makeSentence

--- stringReader.js
+++ stringReader.js
@@ -34,7 +34,7 @@ makes a new array where each element is an object.
     var retArr = [];
     for (var i = 0; i < array.length; i++) {
       var obj = {}
-      var line = array[i].split(split);
+      var line = array[i].split(split)
         obj.str = line[0].trim();
         obj.negated = false;
       if(line[1].trim() === "true"){obj.bool = true;}
@@ -122,3 +122,24 @@ Makes a (likely run-on) sentence out of a conditional stored in an object with t
 @param condition {array} an array of objects formatted as listed above
 @return {object} a new object with the same general format
 */
+
+  }
+  function makeQuestion(conditions, maxDepth=3, negate-.2){
+
+  }
+  /* makeSentence(condition)
+  Makes a (likely run-on) sentence out of a conditional stored in an object with the keys str, bool, and negated.
+  It does the following: if negated then it capitalized the i in "it is not the case" and adds a period to the end of the str.
+  If it is not negated then it adds the phrase "It is the case " to the start of the str and adds a period to the end of the string.
+  @param condition {array} an array of objects formatted as listed above
+  @return {object} a new object with the same general format
+  */
+
+  function makeSentence(condition){
+    if (condition.negated) {
+      condition.str = "I" + condition.str.slice(1) + ".";
+    }else{
+      condition.str = "It is the case " + condition.str + "."
+    }
+    return condition;
+  }
