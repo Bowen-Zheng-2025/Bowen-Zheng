68653952ec58e3e1aaeeab46b5298ebd5aac2686
Bowen Zheng <bowenzheng2003@gmail.com>
2019-10-31 10:04:02 -0400

    aidan thing was 'fixed'

--- Aidan's.js
+++ Aidan's.js
@@ -17,15 +17,13 @@ front of it, sets the negated key to true
 @param condtion {object} an object with str and bool keys
 @return {object} an object with str, bool, and negated keys
 */
-function notEval(con){
-  return {str:"It is not the case that" + con.str, con.bool:!con.bool,con.negated:!con.negated}
-}
+
 /* makeSentence(condition)
 Makes a (likely run-on) sentence out of a conditional stored in an object with the keys str, bool, and negated. It does the following: if negated then it capitalized the i in "it is not the case" and adds a period to the end of the str. If it is not negated then it adds the phrase "It is the case " to the start of the str and adds a period to the end of the string.
 @param condition {array} an array of objects formatted as listed above
 @return {object} a new object with the same general format
 */
   function makeSen(){
-    
+
 
   }
