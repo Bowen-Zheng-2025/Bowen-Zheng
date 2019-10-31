d801496f1e9db58248034afe066fa9f7d961e5f0
Aidan <aidanbab1@gmail.com>
2019-10-31 10:04:26 -0400

    Auto stash before merge of "master" and "origin/master"

--- Aidan's.js
+++ Aidan's.js
@@ -10,20 +10,17 @@ it should randomly use and or ors at a 50% chance each
   function makeQuestion(condition, maxDepth=3,negate =.2){
 
 
-  }
-/* notEval(condition)
-takes a condtion, negates it's value, adds the text "it is not the case that" to the
-front of it, sets the negated key to true
-@param condtion {object} an object with str and bool keys
-@return {object} an object with str, bool, and negated keys
-*/
-
 /* makeSentence(condition)
 Makes a (likely run-on) sentence out of a conditional stored in an object with the keys str, bool, and negated. It does the following: if negated then it capitalized the i in "it is not the case" and adds a period to the end of the str. If it is not negated then it adds the phrase "It is the case " to the start of the str and adds a period to the end of the string.
 @param condition {array} an array of objects formatted as listed above
 @return {object} a new object with the same general format
 */
   function makeSen(){
+    if(condition.negated){
+      condtion.str = "I" + condition.str.slice(1)+".";
+    }else{
+      condition.str = "It is the case" + condtion.str
+    }
 
 
   }
